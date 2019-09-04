42f65f7d189868380c070818c42a7bb89cf09657
xulei2016 <978027105@qq.com>
2019-09-04 10:21:34 +0800

    new UI

--- resources/views/Admin/Base/callCenter/chatRoom/index.blade.php
+++ resources/views/Admin/Base/callCenter/chatRoom/index.blade.php
@@ -3,40 +3,50 @@
 @section("content")
     <link rel="stylesheet" href="{{asset('callCenter/css/chat.css')}}">
 
-    <div class="box box-default">
-        <div class="box-body" style="height: 100%">
-            <div class="row" >
-{{--                客户与客服列表--}}
-                <div class="col-md-2">
-                    <div class="online-users">
-                        <div class="online-users-head">在线客户</div>
-                        <div class="online-users-info">
-                            <ul>
-
-                            </ul>
+    <div class="row" >
+        <div class="col-md-3">
+            <div class="card card-parimary card-outline">
+                <div class="nav-tabs-custom">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item"><a class="nav-link active" href="#kh" data-toggle="tab">在线客户</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#kf" data-toggle="tab">在线客服</a></li>
+                    </ul>
+                    <div class="card-body tab-content">
+                        <div class="tab-pane active" id="kh">
+                            <div class="online-users">
+                                <div class="online-users-head">在线客户</div>
+                                <div class="online-users-info">
+                                    {{--在线客户--}}
+                                    <ul></ul>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="online-operators">
-                        <div class="online-operators-head">在线客服</div>
-                        <div class="online-operators-info">
-                            <ul>
-{{--                                在线客服--}}
-                            </ul>
+                        <div class="tab-pane" id="kf">
+                            <div class="online-operators">
+                                <div class="online-operators-head">在线客服</div>
+                                <div class="online-operators-info">
+                                    {{--在线客服--}}
+                                    <ul></ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-{{--                聊天界面--}}
-                <div class="col-md-7">
+            </div>
+        </div>
+        {{--聊天界面--}}
+        <div class="col-md-6">
+            <div class="card card-primary card-outline">
+                <div class="card-body" style="height: 100%">
                     <div class="talk">
                         <ul class="nav nav-tabs">
-{{--                           tab页--}}
+                        {{--tab页--}}
                         </ul>
                         <div class="chat-content-out">
-{{--                            聊天窗口--}}
+                        {{--聊天窗口--}}
                         </div>
 
                         <div class="footer">
-
                             <div class="customer-chat-emots-menu">
                                 <div class="customer-chat-header-menu-triangle"></div>
                                 <div class="emots-wrapper">
@@ -75,47 +85,36 @@
 
                     </div>
                 </div>
-{{--                客户与客服信息--}}
-                <div class="col-md-3">
-                    <div class="modification-info">
-                        <div class="userMod">
-                            <div class="userMod-header">
-                                客户信息
-                            </div>
+            </div>
+        </div>
+                {{--客户与客服信息--}}
+        <div class="col-md-3">
+            <div class="card card-parimary card-outline">
+                <div class="nav-tabs-custom">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item"><a class="nav-link active" href="#kh_infos" data-toggle="tab">客户信息</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#speed_moudle" data-toggle="tab">快捷模板</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#kf_infos" data-toggle="tab">客服管理</a></li>
+                    </ul>
+                    <div class="card-body tab-content">
+                        <div class="tab-pane active" id="kh_infos">
+                            <ul class="nav nav-pills flex-column">
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">客户姓名:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">资金账号:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">开户日期:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">当日权益:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">客户类型:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">银期关联:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">银期关联:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">手续费标准:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">保证金标准:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">客户姓名:<span class="label label-primary pull-right">暂无</span></a>
+                                    <a href="" class="nav-link">客户姓名:<span class="label label-primary pull-right">暂无</span></a>
+                                </li>
+                            </ul>
                             <div class="userMod-body">
-{{--                                <img id="customer-avatar" src="{{asset('callCenter/img/avatar-2.png')}}" />--}}
-                                <div class="userDiv">
-                                    <label>客户姓名:</label>
-                                    <span id="customer-name">暂无</span>
-                                </div>
-                                <div class="userDiv">
-                                    <label>资金账号:</label>
-                                    <span id="customer-zjzh">暂无</span>
-                                </div>
-                                <div class="userDiv">
-                                    <label>开户日期:</label>
-                                    <span id="customer-khrq">暂无</span>
-                                </div>
-                                <div class="userDiv">
-                                    <label>当日权益:</label>
-                                    <span id="customer-zjqy">暂无</span>
-                                </div>
-                                <div class="userDiv">
-                                    <label>客户类型:</label>
-                                    <span id="customer-client">暂无</span>
-                                </div>
-                                <div class="userDiv">
-                                    <label>银期关联:</label>
-                                    <span id="customer-yq">未知</span>
-                                </div>
-                                <div class="userDiv">
-                                    <label>手续费标准:</label>
-                                    <span id="customer-sxf">暂无</span>
-                                </div>
-                                <div class="userDiv">
-                                    <label>保证金标准:</label>
-                                    <span id="customer-bzj">暂无</span>
-                                </div>
+                                {{--<img id="customer-avatar" src="{{asset('callCenter/img/avatar-2.png')}}" />--}}
                                 <hr />
                                 <div class="userDiv">
                                     <label>交易权限:</label>
@@ -125,13 +124,8 @@
                                     </span>
                                 </div>
                             </div>
-
                         </div>
-
-                        <div class="opMod">
-                            <div class="modification-header">
-                                客服管理
-                            </div>
+                        <div class="tab-pane" id="kf_infos">
                             <div class="modification-body">
                                 <div class="manager-name changeDiv">
                                     <label>昵称:</label>
@@ -148,19 +142,20 @@
                             </div>
                             <input id="manager_id" name="manager_id" value="{{$manager['id']}}"type="hidden" />
                         </div>
-                        <div class="template common-panel">
-                            <div class="common-header">模板语句(常见问题)</div>
-                            <div class="common-body">
-                                <ul>
-{{--                                   模板--}}
-                                </ul>
+                        <div class="tab-pane" id="speed_moudle">
+                            <div class="template common-panel">
+                                <div class="common-header">模板语句(常见问题)</div>
+                                <div class="common-body">
+                                    <ul>
+                                    {{--模板--}}
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="show-template">
+                                <p class="t-title">参考回答</p>
+                                <div></div>
                             </div>
                         </div>
-                        <div class="show-template">
-                            <p class="t-title">参考回答</p>
-                            <div></div>
-                        </div>
-
                     </div>
                 </div>
             </div>
